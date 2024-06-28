8e05f28956d9fc713ae585ce28ed38db66ddeef3
Cheruiyot Evans <chersoftwaredev@gmail.com>
2024-06-28 07:41:11 +0300

    Fixed index.js

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 24;
+let num4 = 5;
+
+const multiply = num1 * num2;
+let random = Math.floor(Math.random() * 1) + 1
+let mod = num3 % num4;
+let max = Math.max(1,15,17,20)
