b081ffc7ddccf0b895946f7d41c5e9679bcb5e1d
Cheruiyot Evans <152331758+CheruiyotDev@users.noreply.github.com.>
2024-09-19 11:15:36 +0300

    Add multiply variable which give the result of multiplication of the two number. add random number generator number greater then zero. add the use of Math.max() function to find the largest number in a set

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
+let num3 = 24
+let num4 = 5
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.floor(Math.random(1) * 100)
+
+const mod = num3 % num4;
+
+const max = Math.max(10, 12, 15, 16, 19 , 20)
